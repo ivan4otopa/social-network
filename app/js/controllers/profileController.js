328e7599f97cc51985ed311cd7640c1e40370d91
Ivan Garnizov <ivan4otopa@abv.bg>
2015-05-26 15:44:58 +0300

    Added news feed view

--- app/js/controllers/profileController.js
+++ app/js/controllers/profileController.js
@@ -1,14 +1,13 @@
 socialNetwork.controller('ProfileController', function ($scope, userService, authenticationService, notifyService) {
-	$scope.getMe = function () {
-		userService.getMe(
+	$scope.getCurrentUserInfo = (function () {
+		userService.getCurrentUserInfo(
 			authenticationService.getCurrentUser().userName,
 			function success(data) {
-				notifyService.showInfo('Got me');
-				$scope.name = data.name;
+				$scope.user = data;
 			},
 			function error(error) {
-				notifyService.showError('Didn\'t get me', error);
+
 			}
 		);
-	};
+	})();
 });
--- app/js/services/userService.js
+++ app/js/services/userService.js
@@ -1,6 +1,6 @@
 socialNetwork.factory('userService', function ($http, baseServiceUrl, authenticationService) {
 	return {
-		getMe: function (username, success, error) {
+		getCurrentUserInfo: function (username, success, error) {
 			var request = {
 				method: 'GET',
 				url: baseServiceUrl + '/api/users/' + username,
